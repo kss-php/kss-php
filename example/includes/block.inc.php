8afb153a9cbfc485f92f26678ccd90ff365252a5
Russell Ahlstrom <russell.ahlstrom@gmail.com>
2013-02-01 09:16:18 -0700

    Updates to BEM Naming Conventions
    Changes the class names found in styleguide.css to use BEM naming
    conventions as outlined at
    http://csswizardry.com/2013/01/mindbemding-getting-your-head-round-bem-syntax.
    
    Updates block.inc.php to use new class names.
    
    Modifies whitespace in layout.css to comply with the other files in
    the project.
    
    Updates buttons.css to follow BEM naming conventions.

--- example/includes/block.inc.php
+++ example/includes/block.inc.php
@@ -1,24 +1,24 @@
-<div class="styleguide-example">
-    <h3>
-        <span class="styleguide-section"><?php echo $section->getSection(); ?></span>
-        <span class="styleguide-title"><?php echo $section->getTitle(); ?></span>
-        <span class="styleguide-filename"><?php echo $section->getFilename(); ?></span>
+<div class="styleguide">
+    <h3 class="styleguide__header">
+        <span class="styleguide__reference"><?php echo $section->getReference(); ?></span>
+        <span class="styleguide__title"><?php echo $section->getTitle(); ?></span>
+        <span class="styleguide__filename"><?php echo $section->getFilename(); ?></span>
     </h3>
 
-    <div class="styleguide-description">
+    <div class="styleguide__description">
         <p><?php echo nl2br($section->getDescription()); ?></p>
         <?php
             if (count($section->getModifiers()) > 0) {
         ?>
-            <ul class="styleguide-modifier">
+            <ul class="styleguide__modifiers">
                 <?php foreach ($section->getModifiers() as $modifier) { ?>
                     <li>
-                        <span class="styleguide-modifier-name <?php ($modifier->isExtender()) ? 'styleguide-extender-name' : ''; ?>">
+                        <span class="styleguide__modifier-name <?php ($modifier->isExtender()) ? 'styleguide__modifier-name--extender' : ''; ?>">
                             <?php echo $modifier->getName(); ?>
                         </span>
                             <?php if ($modifier->isExtender()) { ?>
                                 @extend
-                                <span class="styleguide-modifier-name"><?php echo $modifier->getExtendedClass(); ?></span>
+                                <span class="styleguide__modifier-name"><?php echo $modifier->getExtendedClass(); ?></span>
                             <?php } ?>
                         <?php if (!empty($modifier->getDescription)) { ?>
                             - <?php echo $modifier->getDescription(); ?>
@@ -29,19 +29,19 @@
         <?php } ?>
     </div>
 
-    <div class="styleguide-elements">
-        <div class="styleguide-element">
+    <div class="styleguide__elements">
+        <div class="styleguide__element">
             <?php echo str_replace('$modifierClass', '', $section->getMarkup()); ?>
         </div>
         <?php foreach ($section->getModifiers() as $modifier) { ?>
-            <div class="styleguide-element styleguide-modifier <?php ($modifier->isExtender()) ? 'styleguide-extender' : ''; ?>">
-                <span class="styleguide-modifier-label <?php ($modifier->isExtender()) ? 'styleguide-extender-label' : ''; ?>"><?php echo  $modifier->getName(); ?></span>
+            <div class="styleguide__element styleguide__element--modifier <?php ($modifier->isExtender()) ? 'styleguide__element--extender' : ''; ?>">
+                <span class="styleguide__element__modifier-label <?php ($modifier->isExtender()) ? 'styleguide__element__modifier-label--extender' : ''; ?>"><?php echo  $modifier->getName(); ?></span>
                 <?php echo $modifier->getExampleHtml(); ?>
             </div>
         <?php } ?>
     </div>
 
-    <div class="styleguide-html">
-        <pre class="styleguide-code"><code><?php echo htmlentities($section->getMarkup()); ?></code></pre>
+    <div class="styleguide__html">
+        <pre class="styleguide__code"><code><?php echo htmlentities($section->getMarkup()); ?></code></pre>
     </div>
 </div>
